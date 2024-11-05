90e727cd6e9745fb36301061548338a31e679639
KaKituken <838242595@qq.com>
2024-11-04 22:20:21 -0500

    test base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-const base = process.env.NODE_ENV === 'production' ? '/home/' : '/'
+// const base = process.env.NODE_ENV === 'production' ? '/home/' : '/'
+const base = '/home/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
