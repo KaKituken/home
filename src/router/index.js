d77587087267b9697c00138a1e168b68f320df75
KaKituken <838242595@qq.com>
2024-11-04 22:29:18 -0500

    test hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Gallery from '../views/Gallery.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/home/'), // 使用 hash 模式，并加上 /home/ 前缀
   routes: [
     {
       path: '/',
