0df3fecceeee5d277335087a8e5fa1fd8e25d7d0
KaKituken <838242595@qq.com>
2024-11-04 22:15:58 -0500

    fix: url base for github

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const base = process.env.NODE_ENV === 'production' ? '/home/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,5 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/'
+  base: base
 })
